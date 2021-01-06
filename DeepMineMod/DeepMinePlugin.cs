19a061bdc748681419c2a26014a613765b5270cd
Daniel Lovell <lovell.daniel92@gmail.com>
2021-01-06 03:22:26 -0800

    Attempting to use Harmony Transpiler to patch lines within SetMineable CIL

--- DeepMineMod/DeepMinePlugin.cs
+++ DeepMineMod/DeepMinePlugin.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using BepInEx;
+using Assets.Scripts;
 using Assets.Scripts.Voxel;
+using Assets.Scripts.Objects;
 using HarmonyLib;
 
 namespace DeepMineMod
@@ -8,14 +10,20 @@ namespace DeepMineMod
     [BepInPlugin("org.bepinex.plugins.deepmine", "Deep Mine Mod", "1.0.0.0")]
     public class DeepMinePlugin : BaseUnityPlugin
     {
+
+        void Log(string text)
+        {
+            UnityEngine.Debug.Log("[Deep Mine Mod] " + text);
+        }
         // Awake is called once when both the game and the plug-in are loaded
         void Awake()
         {
-            UnityEngine.Debug.Log("Hello, world!");
-            print(WorldManager.BedrockLevel.ToString());
+            Log("Successfully loaded Deep Mine Mod");
 
+            Log("Patching...");
             var harmony = new Harmony("com.dl.deepmine"); // rename "author" and "project"
             harmony.PatchAll();
+            Log("Patched");
         }
 
     }
--- DeepMineMod/Patches.cs
+++ DeepMineMod/Patches.cs
@@ -1,118 +1,95 @@
 ﻿using System;
+using System.Linq;
 using Assets.Scripts;
 using Assets.Scripts.Voxel;
 using HarmonyLib;
 
 using System.Reflection;
 using System.Collections.Generic;
-using System.IO;
-using Assets.Scripts.GridSystem;
-using Assets.Scripts.Serialization;
-using Assets.Scripts.Util;
 using UnityEngine;
-using UnityEngine.Rendering;
 
+using Assets.Scripts.Objects.Items;
+using System.Reflection.Emit;
 
 namespace DeepMineMod
 {
+    
     [HarmonyPatch(typeof(Asteroid), "SetMineable", new Type[] { typeof(Vector4), typeof(Mineables), typeof(HashSet < ChunkObject >), typeof(int)})]
     public class Asteroid_SetMineable
     {
-        static bool Prefix(Asteroid __instance, ref IReadOnlyCollection<ChunkObject> __result,
-            Vector4 localPosition, Mineables mineable, HashSet<ChunkObject> chunks, int attempts )
+        /**
+         * Patch the Asteroid::SetMineable function (placing mineables on an asteroid, aka chunk)
+         */
+
+        static FieldInfo VeinSizeFieldInfo = AccessTools.Field(typeof(Mineables), "VeinSize");
+        static MethodInfo m_MyExtraMethod = SymbolExtensions.GetMethodInfo((Vector3 x) => CalculateVeinSize(x));
+
+        static float CalculateVeinSize(Vector3 localPos)
         {
-            System.Random _mineableRandom = (System.Random)__instance.GetType().GetField("_mineableRandom", BindingFlags.NonPublic
-                | BindingFlags.Instance).GetValue(__instance);
-            //UnityEngine.Debug.Log("Found _mineableRandom: " + _mineableRandom);
-            if (chunks == null)
-            {
-                // No chunks provided
-                var _chunksInfo = __instance.GetType().GetField("_chunks", BindingFlags.NonPublic | BindingFlags.Instance);
-                var _chunksVal = _chunksInfo.GetValue(__instance);
-                var _chunksType = _chunksInfo.FieldType;
-                var _chunksClearMethod = _chunksType.GetMethod("Clear");
+            float veinSize = localPos.y * (-16.67f) + 166.67f;
+            veinSize = Math.Min(10, Math.Max(0, veinSize));
+            Debug.Log("Transpiler calculated " + veinSize);
+            return veinSize;
+        }
 
-                //  UnityEngine.Debug.Log("Invoking _chunks.Clear(): " );
-                _chunksClearMethod.Invoke(_chunksVal, new object[] { });
-                //UnityEngine.Debug.Log("Invoked _chunks.Clear()");
-                chunks = (HashSet < ChunkObject > )_chunksVal;
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var foundMassUsageMethod = false;
+
+            var instructionList = new List<CodeInstruction>(instructions);
+            
+            int startIndex;
+            for (int i = 0; i < instructionList.Count; i++)
+            {
+                if (instructionList[i].LoadsField(VeinSizeFieldInfo))
+                {
+                    startIndex = i - 1; // Back to nop@ IL012B
+                    // Load V_0 (Grid3 world pos) onto the stack
+                    instructionList[startIndex] = new CodeInstruction(OpCodes.Ldarg_1);
+                    instructionList[startIndex + 1] = new CodeInstruction(OpCodes.Call, m_MyExtraMethod);
+                    
+                }
             }
 
-            //m
-            //UnityEngine.Debug.Log("Invoking localPosition,ToGridRaw()" );
-            Grid3 grid = new Grid3((int)localPosition.x, (int)localPosition.y, (int)localPosition.z);
-            //UnityEngine.Debug.Log("Invoked localPosition,ToGridRaw()");
-            //em
-            mineable = (mineable ?? MiningManager.GenerateRandomMineableType(_mineableRandom));
-            while (attempts < mineable.MaxVeinAttempts)
-            {
-                attempts++;
-                if (!VoxelGrid.IsPositionValid(grid, __instance.ChunkSize))
-                {
-                    // If grid position is invalid on this chunk
-                    Asteroid asteroid = __instance.ChunkController.GetChunk(__instance.LocalPosition + grid.ToVector3Raw()) as Asteroid;
-                    bool flag3 = asteroid == null;
-                    IReadOnlyCollection<ChunkObject> result;
-                    if (flag3)
-                    {
-                        result = chunks;
-                    }
-                    else
-                    {
-                        
-                        grid.ModuloCorrect(__instance.ChunkSize);
-                        //UnityEngine.Debug.Log("Attempting voodoo");
-                        Prefix(__instance, ref __result, localPosition, mineable, chunks, attempts);
-                        //UnityEngine.Debug.Log("Voodoo success");
-                        result = chunks;
-                    }
-                    __result = result;
-                    return false;
-                }
-                chunks.Add(__instance);
-                __instance.SetVoxel((byte)mineable.VoxelType, 1f, (short)grid.x, (short)grid.y, (short)grid.z, true);
-                __instance.PopulateMineable(grid.ToVector3Raw(), mineable.VoxelType);
-                bool flag4 = _mineableRandom.NextDouble() >= (double)(1f - mineable.VeinSize);
-                if (flag4)
-                {
-                    while (attempts < mineable.MaxVeinAttempts)
-                    {
-                        Grid3 grid2;
-                        do
-                        {
-                            grid2.x = _mineableRandom.Next(-1, 2);
-                            grid2.y = _mineableRandom.Next(-1, 2);
-                            grid2.z = _mineableRandom.Next(-1, 2);
-                        }
-                        while (grid2.x == 0 && grid2.y == 0 && grid2.z == 0);
-                        Grid3 grid3 = grid + grid2;
-                        Vector3 worldPosition = __instance.LocalPosition + grid3.ToVector3Raw();
-                        ChunkObject chunk = ChunkController.World.GetChunk(worldPosition);
-                        bool flag5 = chunk != null;
-                        if (flag5)
-                        {
-                            Voxel voxelWorld = __instance.ChunkController.GetVoxelWorld(worldPosition);
-                            bool flag6 = voxelWorld.GetDensityAsFloat() >= __instance.IsoLevel && voxelWorld.Type != byte.MaxValue;
-                            if (flag6)
-                            {
-                                grid = grid3;
-                                break;
-                            }
-                        }
-                        attempts++;
-                    }
-                }
-                else
-                {
-                    bool flag7 = attempts < mineable.MinVeinAttempts;
-                    if (!flag7)
-                    {
-                        break;
-                    }
-                }
-            }
-            __result = chunks;
-            return false;
+            return instructionList.AsEnumerable();
         }
     }
+
+    [HarmonyPatch(typeof(Mineables), MethodType.Constructor, new Type[] { typeof(Mineables), typeof(Vector3), typeof(Asteroid)})]
+    public class Mineables_Constructor
+    {
+        static void Postfix(Mineables __instance, Mineables masterInstance, Vector3 position, Asteroid parentAsteroid)
+        {
+
+            Debug.Log("Mineable vein size before: " + __instance.VeinSize);
+            //__instance.VeinSize = position.y*(-16.67f) + 166.67f;
+            //__instance.VeinSize = Math.Min(10, Math.Max(0, __instance.VeinSize));
+            //Debug.Log(__instance.Position + __instance.DisplayName);
+        }
+    }
+
+
+    [HarmonyPatch(typeof(CursorVoxel), MethodType.Constructor)]
+    public class CursorVoxel_Constructor
+    {
+        static void Postfix(CursorVoxel __instance)
+        {
+            Type typeBoxCollider = __instance.GameObject.GetComponent("BoxCollider").GetType();
+            PropertyInfo prop = typeBoxCollider.GetProperty("size");
+            prop.SetValue(__instance.GameObject.GetComponent("BoxCollider"), new Vector3(5, 5, 5), null);
+        }
+    }
+
+    [HarmonyPatch(typeof(MiningDrill), "Awake")]
+    public class MiningDrill_Awake
+    {
+        static void Prefix(MiningDrill __instance)
+        {
+            __instance.MineCompletionTime = 0.05f;
+            __instance.MineAmount = 0.5f;
+        }
+    }
+
+    //[HarmonyPatch(typeof(TestClass), MethodType.Constructor, new Type[] { typeof(int) })]
+
 }
